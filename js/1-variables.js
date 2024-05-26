05f8e5a0577e83e7073429f407774932162a602b
Yanina Alejandra Rodríguez <121127071+yanina-rodriguez-dev@users.noreply.github.com>
2024-05-26 15:35:53 -0300

    Agrego validacion faltante

--- js/1-variables.js
+++ js/1-variables.js
@@ -1,4 +1,31 @@
 //.- Escribe un programa de una sola línea que haga que 
-//aparezca en la pantalla un alert que diga “un mensaje”.
+//aparezca en la pantalla un alert que diga “un mensaje
 
+//declarar una variable, uso lowercamelcase
 
+let nombreUsuario = 'Gerardo'; 
+ const anio = 2024; 
+//accede a la consola del navegador para que se muetsre el mensaje
+console.log(nombreUsuario);
+console.log('Hola mundo');
+// concateno con el + para unir 
+console.log(nombreUsuario + ' es alumno del año ' + anio);
+
+//concatenar con el $ con bagticks, escribo dentro de las comillas todo:
+console.log(`${nombreUsuario} es alumno del ${anio}`);
+
+// Si imprimo por document.write imprimo una cadena de texto en el documento HTML 
+document.write(nombreUsuario+ 'es alumno del '+ anio);
+//escribir html en el documento 
+
+document.write('<h1 class="tituloPrincipal">Hola Mundo</h1>');
+
+//VENTANAS EMERGENTES: ALERTS
+alert('Este es mi primer alert')
+
+//Modificamos el contenido de la variable 
+ nombreUsuario = 'Mauricio';
+
+ document.write(nombreUsuario + 'Es alumno del año' + anio);
+ document.write( '<br><br/>');
+ 
---
+++ js/2-condicionales.js
@@ -0,0 +1,25 @@
+// Estructura condicionales
+
+//Solicitar la edad del usuario y ostrar por pantalla
+//Pedirle la edad al usuario 
+//puede votar a partir de los 18
+//el prompt es una ventana emergente 
+
+let edad = parseInt(prompt('Ingrese su edad'));
+console.log(typeof(edad));
+
+ //typeOf me sirve para saber el tipo de dato que tiene una variable 
+ 
+// parseInt('40);
+// parseFloat('50.5')
+
+if (edad >=16 &&  edad <=18 || edad >=70){
+    document.write('Es optativo votar');
+}else{
+    if(edad >= 18 && edad <70){
+        document.write('Es obligatorio votar');
+    }else{
+        document.write('No puede votar')
+    }
+    // document.write('Usted no puede votar');
+}
