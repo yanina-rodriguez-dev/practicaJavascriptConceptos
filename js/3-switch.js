b86fc4e4fe56be43afb04ef4b0ba9afbb1a001d7
Yanina Alejandra Rodríguez <121127071+yanina-rodriguez-dev@users.noreply.github.com>
2024-05-27 11:32:29 -0300

    Agregamos estructura de switch

---
+++ js/3-switch.js
@@ -0,0 +1,22 @@
+//especie de menu con if anidados 
+// Si el usuario presiona 1  que haga una cosa , si presiona la opcion 2 otra cosa y 3 otra cosa 
+
+// Si el contenido de la variable opcion es exactamente igual a 1 
+
+/*switch(opcion){
+    case 1:
+        todas las lineas de codigo que quiero hacer si se cumple la opcion 1
+        al final de la estructura ponemos
+        break; 
+    case 2:
+            todas las lineas de codigo que quiero hacer si se cumple la opcion 1
+        al final de la estructura ponemos
+        break; 
+    case 3:
+            todas las lineas de codigo que quiero hacer si se cumple la opcion 1
+        al final de la estructura ponemos
+        break; 
+    default:
+        todas las lineas de caso por defecto    
+}
+*/
