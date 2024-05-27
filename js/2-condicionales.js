07a67f7122390426dffd7ed1fcaaa19418c029d4
Yanina Alejandra Rodríguez <121127071+yanina-rodriguez-dev@users.noreply.github.com>
2024-05-26 21:12:56 -0300

    Agrego validaciones

--- js/2-condicionales.js
+++ js/2-condicionales.js
@@ -12,14 +12,18 @@ console.log(typeof(edad));
  
 // parseInt('40);
 // parseFloat('50.5')
-
-if (edad >=16 &&  edad <=18 || edad >=70){
-    document.write('Es optativo votar');
-}else{
-    if(edad >= 18 && edad <70){
-        document.write('Es obligatorio votar');
+if (edad >0 && edad <130 ){
+    if (edad >=16 &&  edad <=18 || edad >=70){
+        document.write('Es optativo votar');
     }else{
-        document.write('No puede votar')
+        if(edad >= 18 && edad <70){
+            document.write('Es obligatorio votar');
+        }else{
+            document.write('No puede votar')
+        }
+        // document.write('Usted no puede votar');
     }
-    // document.write('Usted no puede votar');
+}else{
+    document.write('No ingreso un valor valido');
 }
+
