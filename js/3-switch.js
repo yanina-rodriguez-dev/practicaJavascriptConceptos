46cbff5f813fe287a5cba9c78f4654cd5e07eb5d
Yanina Alejandra Rodríguez <121127071+yanina-rodriguez-dev@users.noreply.github.com>
2024-05-28 00:20:09 -0300

    Agrego un ejercicio de menu de un bar

--- js/3-switch.js
+++ js/3-switch.js
@@ -20,3 +20,40 @@
         todas las lineas de caso por defecto    
 }
 */
+
+// Solicitar al usuario que seleccione un dia de la semana y le vamos a sugerir un menu para cada dia 
+// cada dia: plato principal, bebida y postre 
+
+//  Como le solicito al usuario un dato? con prompt 
+//  a donde se va a guardar el resultado ? en una variable const  
+ 
+const dia = prompt('Selecione un dia de la semana 1-Lunes,2-Miercoles , 3-Viernes');
+
+switch(dia){
+case '1':
+    document.write(`<h2>Menu del lunes</h2>
+    <ul>
+    <li>Plato principal: Pizza</li>
+    <li>Bebida: Cerveza</li>
+    <li>Postre:Panqueque con dulced de leche</li>
+    </ul>`);
+    break;
+case '2':
+    document.write(`<h2>Menu del miercoles</h2>
+    <ul>
+    <li>Plato principal: Langostinos con salsa de camaron</li>
+    <li>Bebida: Vino</li>
+    <li>Postre:Tiramisu</li>
+    </ul>`)
+    break;
+    case '3':
+    document.write(`<h2>Menu del viernes</h2>
+    <ul>
+    <li>Plato principal: Empanadas</li>
+    <li>Bebida: Vino</li>
+    <li>Postre:Helado</li>
+    </ul>`)
+    break;
+    default:
+        document.write('No ingreso un valor correcto')
+}
